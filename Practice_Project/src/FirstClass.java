ca294a44f1faf40e27564efe2baba1b502e9752e
Soumya Rajidi <soumya83.rajidi@gmail.com>
2019-11-05 12:14:01 +0000

    changes made

--- Practice_Project/src/FirstClass.java
+++ Practice_Project/src/FirstClass.java
@@ -3,7 +3,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.printil("this is the change i made in your code");
+System.out.printil("this is the changes i made in your code");
 
 	}
 
